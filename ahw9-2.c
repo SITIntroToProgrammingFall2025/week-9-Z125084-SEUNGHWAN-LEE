36790bbce97a2e620e90cbe59be430bf0f4cfe5b
Z125084-SEUNGHWAN-LEE <z125084@shibaura-it.ac.jp>
2025-12-09 17:04:21 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    const int SIZE = 5;
+    int arr[5];
+    int i, j, temp;
+
+    for (i = 0; i < SIZE; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < SIZE - 1; i++) {
+        int min_index = i;
+        for (j = i + 1; j < SIZE; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        
+        if (min_index != i) {
+            temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
